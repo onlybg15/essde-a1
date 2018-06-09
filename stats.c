cdfa7f78d53e88ab24bf652703643940be0d76d9
Bryan Grant <bryan.grant@gmail.com>
2018-06-09 10:56:39 -0600

    Adding implemented code for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -19,7 +19,7 @@
  * nearest integer.
  *
  * @author Bryan Grant
- * @date 6/6/2018
+ * @date 6/9/2018
  *
  */
 
@@ -41,33 +41,85 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+  printf("Unsorted Array:\n");
+  print_array(test, SIZE);
+
+  printf("\nSorted Array:\n");
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+
+  print_statistics(test, SIZE);
 
 }
 
 void print_statistics(unsigned char * array, unsigned int length) {
-
+  printf("\nArray Statistics:\n");
+  printf("Median: %d\n", find_median(array,length));
+  printf("Mean: %d\n", find_mean(array,length));
+  printf("Maximum: %d\n", find_maximum(array,length));
+  printf("Minimum: %d\n", find_minimum(array,length));
 }
 
 void print_array(unsigned char * array, unsigned int length) {
-
+  for (int i=0; i<length; i++) {
+    printf("Index: %d --- Value: %d\n", i, array[i]);
+  }
 }
 
 unsigned char find_median(unsigned char * array, unsigned int length) {
+  unsigned char median = 0;
+  unsigned int mindex = length/2;
 
+  sort_array(array, length);
+
+  if (length%2==0) {
+    median = (array[mindex] + array[mindex-1])/2;
+  } else {
+    median = array[mindex];
+  }
+  //printf("%d, %d\n", mindex, median);
+  return median;
 }
 
 unsigned char find_mean(unsigned char * array, unsigned int length) {
-
+  unsigned char mean = 0;
+  unsigned int sum = 0;
+  for (int i=0; i<length; i++) {
+    sum += array[i];
+  }
+  mean = (unsigned char)(sum/length);
+  //printf("%d, %d\n", sum, length);
+  return mean;
 }
 
 unsigned char find_maximum(unsigned char * array, unsigned int length) {
-
+  unsigned char max = 0;
+  for (int i=0; i<length; i++) {
+    if (i==0) max = array[i];
+    else if (array[i]>max) max = array[i];
+  }
+  return max;
 }
 
 unsigned char find_minimum(unsigned char * array, unsigned int length) {
-
+  unsigned char min = 0;
+  for (int i=0; i<length; i++) {
+    if (i==0) min = array[i];
+    else if (array[i]<min) min = array[i];
+  }
+  return min;
 }
 
 void sort_array(unsigned char * array, unsigned int length) {
-
+  if (length == 1) return;
+  else {
+    for (int i=0; i<length-1; i++) {
+      if (array[i]<array[i+1]) {
+        unsigned char swap = array[i];
+        array[i] = array[i+1];
+        array[i+1] = swap;
+      }
+    }
+    sort_array(array, length-1);
+  } 
 }
