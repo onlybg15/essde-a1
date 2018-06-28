86310b441e68f5ea610dcc911568edb35137a58f
Bryan Grant <bryan.grant@gmail.com>
2018-06-27 19:09:05 -0600

    Reconfiguring repository for Course 1 Final Assignment

---
+++ course1/src/data.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file data.c
+ * @brief Analyze array of data and provide some statistical analysis.
+ *
+ * This program will take in an array of unsigned char data items and report
+ * analytics on the maximum, minimum, mean, and median. This program will also
+ * sort the data from largest to smallest present the data to the screen in a 
+ * nicely formatted presentation. All statistics will be rounded down to the 
+ * nearest integer.
+ *
+ * @author Bryan Grant
+ * @date 6/27/2018
+ *
+ */
